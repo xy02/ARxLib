3844d46e26ce0879be24813127e3cca7b142e3de
xy02 <xyxy0202@qq.com>
2018-08-17 10:51:25 +0800

    fix

--- app/src/main/java/com/github/xy02/arxlib/ARxClient.java
+++ app/src/main/java/com/github/xy02/arxlib/ARxClient.java
@@ -10,12 +10,15 @@ import android.util.Log;
 
 import com.alibaba.fastjson.JSON;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import io.reactivex.Observable;
+import io.reactivex.Observer;
 import io.reactivex.annotations.Nullable;
-import io.reactivex.disposables.Disposable;
 import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.Subject;
 
 /**
  * Created by xy on 18-5-7.
@@ -27,6 +30,7 @@ public class ARxClient {
 
     private Intent intent;
     private Context context;
+    private Map<String, Observer<String>> observerMap = new HashMap<>();
 
     public ARxClient(Context context, String packageName, String action) {
         this.intent = new Intent(action);
@@ -36,15 +40,18 @@ public class ARxClient {
 
     public Observable<String> callARxService(String apiName, @Nullable String reqJSONData) {
         return observableIARxService
-                .flatMap(service->{
+                .take(1)
+                .flatMap(service -> {
                     String id = UUID.randomUUID().toString();
-                    service.call(id, apiName, reqJSONData, serviceCb);
-                    return onNextSubject
-                            .filter(it -> it.id.equals(id))
-                            .map(it -> it.message)
-                            .takeUntil(onFinishSubject
-                                    .filter(it -> it.id.equals(id))
-                            );
+                    Subject<String> sub = PublishSubject.create();
+                    observerMap.put(id, sub);
+                    return sub
+                            .doFinally(() -> observerMap.remove(id))
+                            .mergeWith(Observable.create(emitter -> {
+                                service.call(id, apiName, reqJSONData, serviceCb);
+                                emitter.onComplete();
+                            }))
+                            ;
                 });
     }
 
@@ -57,33 +64,24 @@ public class ARxClient {
                 .map(it -> JSON.parseObject(it, observableDataClazz));
     }
 
-    private class CbData {
-         String id;
-         String message;
-
-         CbData(String id, String message) {
-            this.id = id;
-            this.message = message;
-        }
-    }
-
-    private class ConnectionState{
+    private class ConnectionState {
         ServiceConnection conn;
         IBinder.DeathRecipient dr;
         IARxService service;
-        void clear(){
-            if (service!=null)
-                service.asBinder().unlinkToDeath(dr,0);
-            if(conn!= null)
+
+        void clear() {
+            if (service != null)
+                service.asBinder().unlinkToDeath(dr, 0);
+            if (conn != null)
                 context.unbindService(conn);
         }
     }
 
     private Observable<IARxService> observableIARxService = Observable.<ConnectionState>create(emitter -> {
         ConnectionState state = new ConnectionState();
-        state.dr = ()->{
+        state.dr = () -> {
             state.clear();
-            emitter.tryOnError(new Exception("service:"+intent.getPackage()+":"+intent.getAction()+" died"));
+            emitter.tryOnError(new Exception("service:" + intent.getPackage() + ":" + intent.getAction() + " died"));
         };
         state.conn = new ServiceConnection() {
 
@@ -91,7 +89,7 @@ public class ARxClient {
             public void onServiceConnected(ComponentName name, IBinder service) {
                 Log.i(tag, "onServiceConnected, tid:" + Thread.currentThread().getId());
                 try {
-                    service.linkToDeath(state.dr,0);
+                    service.linkToDeath(state.dr, 0);
 //                    arxService.call(id, apiName, reqJSONData, serviceCb);
                 } catch (RemoteException ex) {
                     emitter.tryOnError(ex);
@@ -114,27 +112,30 @@ public class ARxClient {
             emitter.tryOnError(new Exception(ERR_CONNECT));
         }
     })
-            .map(state-> state.service)
+            .map(state -> state.service)
             .replay(1)
             .refCount();
-
-    private PublishSubject<CbData> onNextSubject = PublishSubject.create();
-    private PublishSubject<CbData> onFinishSubject = PublishSubject.create();
 
     private IObservableEmitter.Stub serviceCb = new IObservableEmitter.Stub() {
         @Override
         public void onNext(String id, String jsonData) throws RemoteException {
-            onNextSubject.onNext(new CbData(id, jsonData));
+            Observer<String> o = observerMap.get(id);
+            if (o != null)
+                o.onNext(jsonData);
         }
 
         @Override
         public void onError(String id, String errMessage) throws RemoteException {
-            onFinishSubject.onNext(new CbData(id, errMessage));
+            Observer<String> o = observerMap.get(id);
+            if (o != null)
+                o.onError(new Exception(errMessage));
         }
 
         @Override
         public void onComplete(String id) throws RemoteException {
-            onFinishSubject.onNext(new CbData(id, null));
+            Observer<String> o = observerMap.get(id);
+            if (o != null)
+                o.onComplete();
         }
     };
 
